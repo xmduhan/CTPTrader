bab976c15b526deb4d453de30ab1d7fc75e32f96
xmduhan <xmduhan@gmail.com>
2015-06-18 10:30:55 +0800

    ...

---
+++ draft.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+1、为DepthMarketData增加admin修改界面
+2、思考目前都需要那些config数据,并初步将模型设计
+3、编写脚本讲ctp数据流保存到数据库
---
+++ task.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+# 导入环境变量
+import sys, os
+path = r'/home/duhan/github/CTPTrader'  # 项目位置
+#path = r'/home/wx/pitchersite'  # 项目位置
+settings = "CTPTrader.settings"
+sys.path.append(path)
+os.chdir(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+
+import os
+from pyctp.CTPChannel import MdChannel
+from datetime import datetime
+from data.models import DepthMarketData
+from dateutil.relativedelta import relativedelta
+
+
+# 读取环境变量
+frontAddress = os.environ.get('CTP_FRONT_ADDRESS')
+mdFrontAddress = os.environ.get('CTP_MD_FRONT_ADDRESS')
+brokerID = os.environ.get('CTP_BROKER_ID')
+userID = os.environ.get('CTP_USER_ID')
+password = os.environ.get('CTP_PASSWORD')
+instrumentID = datetime.strftime(datetime.now() + relativedelta(months=1),"IF%y%m")
+#frontAddress,mdFrontAddress,brokerID,userID,password,instrumentID
+
+#
+mdChannel = MdChannel(mdFrontAddress,brokerID,userID,password,\
+    ['IF1506','IF1507','IF1508','IF1509','IF1510','IF1511','IF1512'])
+i = 0
+while True:
+    result = mdChannel.readMarketData(1000)
+    if result:
+        DepthMarketData(**result.toDict()).save()
+        i += 1
+        print u'成功第%d条数据' % i
+    else:
+        print u'跳过...'
