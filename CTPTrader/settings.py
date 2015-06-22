2efd706adb7b1334104fb562c770563b0d0e4ea5
xmduhan <xmduhan@gmail.com>
2015-06-22 09:56:58 +0800

    ...

--- CTPTrader/settings.py
+++ CTPTrader/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'config',
-    'data'
+    'database'
 )
 
 MIDDLEWARE_CLASSES = (
